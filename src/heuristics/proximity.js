b88b9901fd3b82385e22226ce8e49f9b662647cd
Firejoust <42525540+Camezza@users.noreply.github.com>
2021-10-17 18:52:59 +1100

    Implement proximity heuristic WIP

--- src/heuristics/proximity.js
+++ src/heuristics/proximity.js
@@ -6,4 +6,11 @@ class proximityHeuristic extends costHeuristic {
     constructor(weighting) {
         super(weighting);
     }
+
+    determineCost(yaw, destination) {
+        if (!this.bot) throw Error("No client has been assigned!");
+        let d = destination.minus(this.bot.entity.position);
+        let r = (Math.atan2(d.z, d.x) - Math.PI) - yaw;
+        return this.weighting * Math.abs(r/Math.PI); // get a ratio of required yaw / current yaw
+    }
 }
