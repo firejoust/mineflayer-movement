c69ecf2a936030f7bea2e5523cef30598b3fc751
Firejoust <42525540+Camezza@users.noreply.github.com>
2021-10-17 18:52:05 +1100

    Create heuristic algorithm

--- index.js
+++ index.js
@@ -1,40 +1,56 @@
-/**
- * @typedef dangerOptions
- * @property multiplier
- * @property maxX
- * @property maxY
- */
-
-/**
- * @typedef distanceOptions
- * @property multiplier
- * @property maxX
- * @property rayTotal (ODD)
- */
-
-/**
- * @typedef proximityOptions
- * @property multiplier
- */
-
-function inject(bot, options) {
-    bot.movement = new plugin(bot, options);
-}
+const assert = require(`assert`);
+const evaluation = require(`./src/util/evaluation`);
 
 class plugin {
-    constructor(bot, options) {
+    constructor(bot) {
         this.bot = bot;
-        this.options = options;
+    }
+
+    steerAngle(destination, heuristics, options) {
+        let rotations = options.rotations || 8; // by default, calculate heuristics for 8 rotational angles
+        let eval = options.evaluation || 'cheapest'; // can also be 'average'
+
+        assert.ok(heuristics.length > 0, "At least one heuristic must be specified.");
+        assert.ok(rotations > 0, "Rotations must be higher than zero.");
+        assert.ok(evaluation[eval], "Invalid evaluation method specified. Must be either \"cheapest\" or \"average\".");
+
+        // assign bot class required to calculate heuristic costs
+        for (let h of heuristics) {
+            h.assignClient(this.bot);
+        }
+
+        let angles = [];
+        let costs = [];
+
+        // calculate the cost of each yaw rotation
+        for (let r = 0, yaw = this.bot.yaw + Math.PI; r < rotations; r++) {
+            let a = yaw + (r/rl) * 2 * Math.PI;
+            let c = 0; // total cost
+
+            // find the total cost from applying heuristics
+            for (let h of heuristics) {
+                c += h.determineCost(a, destination);
+            }
+
+            angles.push(a);
+            costs.push(c);
+        }
+
+        // find the optimal angle from costs
+        return evaluation[eval](costs, angles);
     }
 }
 
-/*
-**  bot.movement.set(options);
-**  // updates currently stored values
-**
-**  bot.movement.follow(positionfunc);
-**  // begins following a dynamic position returned from the specified function
-**
-**  bot.movement.stop();
-**  // stops following the target
-*/
+function inject(bot) {
+    bot.movement = new plugin(bot);
+}
+
+module.exports = {
+    plugin: inject,
+    heuristics: {
+        genericHeuristic: require(`./src/heuristics/heuristic`),
+        dangerHeuristic: require(`./src/heuristics/danger`),
+        distanceHeuristic: require(`./src/heuristics/distance`),
+        proximityHeuristic: require(`./src/heuristics/proximity`),
+    }
+}
---
+++ src/util/evaluation.js
@@ -0,0 +1,25 @@
+function cheapest(costs, angles) {
+    let c;
+    for (let i = 0, il = costs.length; i < il; i++) {
+        if (!c || costs[i] < c[i]) c = i;
+    }
+    return angles[c];
+}
+
+function average(costs, angles) { 
+    let x, z;
+    x = z = 0;
+
+    // add up all the angle differences to determine the center
+    for (let i = 0, il = costs.length; i < il; i++) {
+        z += costs[i] * Math.cos(angles[i]);
+        x += costs[i] * Math.sin(angles[i]);
+    }
+
+    return Math.atan2(x, z);
+}
+
+module.exports = {
+    cheapest,
+    average,
+}
