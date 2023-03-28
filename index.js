ef7e6790e5e8eb4daa065eaf7f516e4942e32f91
firejoust <42525540+firejoust@users.noreply.github.com>
2023-03-28 20:04:06 +1100

    use boundingbox offset

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const Heuristics = require("./src/heuristics")
+const Angle = require("./src/angle")
+
+const States = [ "back", "left", "forward", "right" ]
+const PI_DOUBLE = Math.PI * 2
 
 module.exports.plugin = function inject(bot) {
     bot.movement = new Plugin(bot)
@@ -12,12 +16,37 @@ function Plugin(bot) {
     this.getYaw        = getYaw
     this.steer         = steer
 
-    function setControls(yaw, angleRadius) {
+    function setControls(yaw, _angleRadius) {
+        const angleRadius = Math.PI * ((_angleRadius || 100) / 180)
+        const diff0 = Angle.difference(bot.entity.yaw, yaw % PI_DOUBLE)
+        const diff1 = Angle.inverse(diff0)
 
+        let index = 0
+
+        // create a radius for each cardinal direction
+        for (let i = -Math.PI; i < Math.PI; i += Math.PI/2) {
+
+            // original angle radius
+            let x0, x1
+            x0 = i - angleRadius / 2
+            x1 = i + angleRadius / 2
+
+            // enable control state if destination angle within radius
+            x0 <= diff0 && diff0 <= x1 || x1 <= diff0 && diff0 <= x0 ||
+            x1 <= diff1 && diff1 <= x0 || x0 <= diff1 && diff1 <= x1
+            ? bot.setControlState(States[index], true)
+            : bot.setControlState(States[index], false)
+            // next state
+            index++
+        }
     }
 
-    function getYaw(fov, rotations) {
-        const costs = new Float64Array(rotations)
+    function getYaw(_fov, _rotations, _blend) {
+        const fov = _fov || 240
+        const rotations = _rotations || 15
+        const blend = _blend || 2
+
+        let costs = new Float64Array(rotations)
 
         // calculate the angular components
         const total = fov * Math.PI / 180
@@ -31,73 +60,51 @@ function Plugin(bot) {
             }
         }
 
-        let cheapest = null
+        // blend the adjacent costs together
+        {
+            const average = new Float64Array(rotations)
+            const total = blend * 2 + 1
+            const half = blend + 1
+            const max = rotations - blend - 1
 
-        // find the rotation with the cheapest cost
-        for (let i = 0; i < rotations; i++) {
-            if (cheapest === null || costs[i] < costs[cheapest]) {
-                cheapest = i
+            for (let i = 0; i <= max; i++) {
+                for (let j = 1; j <= blend; j++)
+                    average[i] += costs[i + j]
+            }
+
+            for (let i = blend; i < rotations; i++) {
+                for (let j = 1; j <= blend; j++)
+                    average[i] += costs[i - j]
+            }
+
+            for (let i = 0; i < rotations; i++) {
+                average[i] += costs[i]
+                // use the correct mean total
+                if (blend <= i && i <= max) {
+                    costs[i] = average[i] / total
+                } else {
+                    costs[i] = average[i] / half
+                }
             }
         }
 
-        return base + increment * cheapest
+        // get the cheapest cost
+        {
+            let cheapest = null
+
+            // find the rotation with the cheapest cost
+            for (let i = 0; i < rotations; i++) {
+                if (cheapest === null || costs[i] < costs[cheapest]) {
+                    cheapest = i
+                }
+            }
+
+            return base + increment * cheapest
+        }
     }
 
-    function steer(yaw, force) {
+    function steer(yaw, _force) {
+        const force = _force || true
         return bot.look(yaw, bot.entity.pitch, force)
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const bot = {}
-
-// FOV?
-
-bot.movement.setHeuristics(
-    new bot.movement.heuristics.Distance(1)
-        .radius(4)
-        .count(3)
-        .spread(20),
-    new bot.movement.heuristics.Danger(1)
-        .radius(2)
-        .depth(5)
-        .spread(0.25),
-    new bot.movement.heuristics.Proximity(2)
-        .target(position)
-        .avoid(true)
-)
-
-const yaw = bot.movement.getYaw(fov, rotations)
-bot.movement.setControls(yaw, angleRadius)
-bot.movement.steer(yaw, forceLook)
-*/
--- src/angle.js
+++ src/angle.js
@@ -1,8 +1,10 @@
+const PI_DOUBLE = Math.PI * 2
+
 // returns an angle's inverse, assuming it is between 0 and 360
 module.exports.inverse = function(angle) {
     return angle < 0
-    ? angle + Math.PI * 2
-    : angle - Math.PI * 2
+    ? angle + PI_DOUBLE
+    : angle - PI_DOUBLE
 }
 
 // returns the smaller difference between two angles
