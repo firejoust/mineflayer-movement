654e9b469753786c63e8e335ed936eeafe97dde3
firejoust <42525540+firejoust@users.noreply.github.com>
2023-04-06 14:14:32 +1000

    change defaults

--- src/goals.js
+++ src/goals.js
@@ -1,32 +1,8 @@
 module.exports = function Goals(bot) {
     this.Default = new bot.movement.Goal({
-        distance: bot.movement.heuristic.new('distance')
-            .configure({
-                weight: 0.4,
-                radius: 10,
-                spread: 25,
-                offset: 1,
-                count: 5,
-                increment: 0.2
-            }),
-        danger: bot.movement.heuristic.new('danger')
-            .configure({
-                weight: 0.6,
-                radius: 3,
-                count: 6,
-                depth: 2,
-                descend: false,
-                increment: 0.2
-            }),
-        proximity: bot.movement.heuristic.new('proximity')
-            .configure({
-                weight: 0.6,
-                avoid: false
-            }),
-        conformity: bot.movement.heuristic.new('conformity')
-            .configure({
-                weight: 0.2,
-                avoid: false
-            })
+        'distance': bot.movement.heuristic.new('distance'),
+        'danger': bot.movement.heuristic.new('danger'),
+        'proximity': bot.movement.heuristic.new('proximity'),
+        'conformity': bot.movement.heuristic.new('conformity')
     })
 }
