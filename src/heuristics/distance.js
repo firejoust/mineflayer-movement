8095f3c3ead041fda204e1ac0b91242b3348953d
firejoust <42525540+firejoust@users.noreply.github.com>
2023-04-06 11:27:21 +1000

    implement new distance heuristic

--- src/heuristics/distance.js
+++ src/heuristics/distance.js
@@ -1,21 +1,15 @@
 const Vec3 = require("vec3")
 
-const PI_RATIO = Math.PI / 180
-
 module.exports.inject = function inject(bot, Set) {
     return class Distance {
-        #weight    = 0.4
-        #radius    = 10
-        #spread    = 40
-        #offset    = 0
-        #count     = 5
+        #weight = 0.4
+        #radius = 10
+        #step   = 1
         #increment = 0.2
 
-        weight    = Set(this, weight => this.#weight = weight)
-        radius    = Set(this, radius => this.#radius = radius)
-        spread    = Set(this, spread => this.#spread = spread)
-        offset    = Set(this, offset => this.#offset = offset)
-        count     = Set(this, count => this.#count = count)
+        weight = Set(this, weight => this.#weight = weight)
+        radius = Set(this, radius => this.#radius = radius)
+        //step   = Set(this, step => this.#step = step) // will be supported later
         increment = Set(this, increment => this.#increment = increment)
 
         configure = Set(this, object => {
@@ -29,83 +23,83 @@ module.exports.inject = function inject(bot, Set) {
         })
 
         cost(yaw) {
-            let cost = 0
-            const vectors1 = new Array(this.#count) // raycast direction
-
             const x = -Math.sin(yaw)
             const z = -Math.cos(yaw)
-            const box = new Float64Array(2) // boundingbox offset
+            const box = new Float64Array(2)
 
-            // allocate boundingbox offset vector
-            if (Math.abs(x) > Math.abs(z)) {
-                box[0] = Math.sign(x)
-                box[1] = z * Math.abs(1 / x)
-            } else {
-                box[0] = x * Math.abs(1 / z)
-                box[1] = Math.sign(z)
-            }
-
-            box[0] *= 0.3
-            box[1] *= 0.3
-
+            // initialise player bounding box offset
             {
-                // angular components
-                const total = this.#spread * PI_RATIO
-                const increment = total / this.#count
-
-                // initialise relative vectors
-                for (let i = 0; i < this.#count; i++) {
-                    vectors1[i] = new Float64Array(3)
-
-                    const theta = increment * i
-
-                    // get the horizontal/vertical 2D components
-                    const h = Math.cos(theta)
-                    const v = Math.sin(theta)
-
-                    // raycast vector components
-                    const x1 = x * h
-                    const z1 = z * h
-
-                    vectors1[i] = [x1, v, z1]
+                if (Math.abs(x) > Math.abs(z)) {
+                    box[0] = Math.sign(x)
+                    box[1] = z * Math.abs(1 / x)
+                } else {
+                    box[0] = x * Math.abs(1 / z)
+                    box[1] = Math.sign(z)
                 }
+
+                box[0] *= 0.3
+                box[1] *= 0.3
             }
 
+            // find where the raycast intercepts with blocks
             {
-                // find where the raycasts intercept with blocks
-                for (let i = 0; i < this.#count; i++) {
+                let y = 0
 
-                    const length = this.#radius / this.#increment
+                // how many iterations will be executed
+                const length = this.#radius / this.#increment
 
-                    // initialise raycast iterator
-                    for (let j = 0; j < length; j++) {
-                        const pos = new Vec3(
-                            vectors1[i][0] * this.#increment * j,
-                            vectors1[i][1] * this.#increment * j,
-                            vectors1[i][2] * this.#increment * j
+                // floor the player's y position to prevent partial block checking
+                const position = new Vec3(
+                    bot.entity.position.x,
+                    Math.floor(bot.entity.position.y),
+                    bot.entity.position.z
+                )
+
+                for (let j = 0; j < length; j++) {
+                    const offset = new Float64Array(2)
+                    offset[0] = x * this.#increment * j
+                    offset[1] = z * this.#increment * j
+
+                    // get absolute positional offset
+                    const pos = position.offset(offset[0], y, offset[1])
+
+                    // set boundingbox offset (half player width)
+                    pos.x += box[0]
+                    pos.z += box[1]
+                    
+                    // check the raycast at step height for an intercept
+                    if (bot.blockAt(pos.offset(0, this.#step, 0))?.boundingBox === 'block') {
+                        return this.#weight * (1 - Math.sqrt(offset[0] ** 2, offset[1] ** 2) / this.#radius)
+                    } else
+
+                    // check the raycast below it to determine if a block can be climbed
+                    if (bot.blockAt(pos)?.boundingBox === 'block') {
+                        const height = new Float64Array(2)
+                        height[0] = x * this.#increment * (j - 1)
+                        height[1] = z * this.#increment * (j - 1)
+
+                        // position of the block above player's head
+                        const lastPos = position.offset(
+                            height[0],
+                            y + this.#step + 1,
+                            height[1]
                         )
 
-                        // get absolute positional offset
-                        const offset = bot.entity.position.plus(pos)
+                        lastPos.x += box[0]
+                        lastPos.z += box[1]
 
-                        // set boundingbox offset (half player width)
-                        offset.x += box[0]
-                        offset.z += box[1]
-
-                        // configurable vertical offset
-                        offset.y += this.#offset
-                        
-                        if (bot.blockAt(offset)?.boundingBox === 'block') {
-                            cost += 1 - Math.sqrt(pos.x ** 2 + pos.y ** 2 + pos.z ** 2) / this.#radius
-                            break
+                        // check block above the player's head, otherwise climb the block
+                        if (bot.blockAt(lastPos)?.boundingBox === 'block') {
+                            return this.#weight * (1 - Math.sqrt(offset[0] ** 2, offset[1] ** 2) / this.#radius)
+                        } else {
+                            y += this.#step
                         }
                     }
                 }
-
-                cost /= this.#count
             }
 
-            return cost * this.#weight
+            // no intercepts
+            return 0
         }
     }
 }
